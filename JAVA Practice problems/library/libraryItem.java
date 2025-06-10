5c608f6607b903628f0bd2ff3703c96ef12f32a6
Sharique Raza <shariqueraza2333@gmail.com>
2025-06-10 19:56:22 +0530

    using extends

---
+++ JAVA Practice problems/library/libraryItem.java
@@ -0,0 +1,16 @@
+package library;
+
+public class libraryItem {
+    private String itemId;
+    private String title;
+    private String author;
+
+
+    public void chekout() {
+        System.out.println("CHECKOUT");
+    }
+
+    public void returnItem() {
+        System.out.println("RETURNED");
+    }
+}
